ec6a782971e0c50f086e0bbb6676b8b45aaa78d6
unknown <student1@FB-W217-W8.academic.touro.edu>
2015-09-17 19:13:56 -0400

    Modified ScrabbleDictionary

--- src/neustadt/scrabble/ScrabbleDictionary.java
+++ src/neustadt/scrabble/ScrabbleDictionary.java
@@ -2,14 +2,14 @@ package neustadt.scrabble;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ScrabbleDictionary {
-	private ArrayList<String> dictionary;
+	private HashSet<String> dictionary;
 
 	public ScrabbleDictionary() throws FileNotFoundException {
-		this.dictionary = new ArrayList<String>();
+		this.dictionary = new HashSet<String>();
 		Scanner readFile = new Scanner(new File("./US.dic"));
 		while (readFile.hasNext()) {
 			dictionary.add(readFile.next());
